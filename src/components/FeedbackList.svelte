f31bad429139cc388097639204043673237cd728
clpowe <clpowe@gmail.com>
2022-03-01 12:46:48 -0500

    Add store

--- src/components/FeedbackList.svelte
+++ src/components/FeedbackList.svelte
@@ -1,8 +1,19 @@
 <script>
+	// import { onMount, onDestroy } from 'svelte'
+	import { FeedbackStore } from '../stores'
+	import { fade, scale } from 'svelte/transition'
 	import FeedbackItem from './FeedbackItem.svelte'
-	export let feedback = []
+	// export let feedback = []
+
+	// const unsubscribe = FeedbackStore.subscribe((data) => (feedback = data))
+
+	// onDestroy(() => {
+	// 	unsubscribe()
+	// })
 </script>
 
-{#each feedback as fb (fb.id)}
-	<FeedbackItem item={fb} on:delete-feedback />
+{#each $FeedbackStore as fb (fb.id)}
+	<div in:scale out:fade={{ duration: 500 }}>
+		<FeedbackItem item={fb} />
+	</div>
 {/each}
